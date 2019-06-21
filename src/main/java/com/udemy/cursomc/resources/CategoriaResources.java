8c77419133bd7c0f0c14f7920d2825b44e0a3453
José Araújo <aderivan.mkt@gmail.com>
2019-06-20 23:55:00 -0300

    Criando repository e service para categoria

--- src/main/java/com/udemy/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResources.java
@@ -1,13 +1,14 @@
 package com.udemy.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaService;
 
 //Package responsável pelo controle do rest
 
@@ -15,15 +16,14 @@ import com.udemy.cursomc.domain.Categoria;
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+	@Autowired
+	public CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	//o pathVariable é para informar que o id que eu irei receber na url, é o id do parâmetro
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
 		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-		
-		return categorias;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
